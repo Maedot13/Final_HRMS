c21c0009fd3515c9e52fbc908dd754119236dc8e
maedot <madotdemelash664@gmail.com>
2026-05-17 00:45:37 +0300

    chore: remove dev scratch scripts before merge to main

--- test-payroll.ts
+++
@@ -1,2 +0,0 @@
-import { getLeavePayrollTransfers } from './packages/backend/src/services/payroll.service';
-getLeavePayrollTransfers().then(res => console.log(JSON.stringify(res, null, 2))).catch(console.error);
